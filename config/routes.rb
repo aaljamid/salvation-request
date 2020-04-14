eceab7102ec537479c90d1d6872058b76c9941b5
aaljamid <aaljamid@emich.edu>
2020-04-14 18:37:34 +0300

    add seeds and add the index for requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  root 'requests#index'
+  
+  root 'pages#home'
+  get 'pages/home'
+  
   resources :requests
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
